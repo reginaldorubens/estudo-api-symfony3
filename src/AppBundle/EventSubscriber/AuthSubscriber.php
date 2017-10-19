3588c8345096fde382fff5d141d12671b1a9aaf0
Reginaldo Rubens da Silva <reginaldorubens@gmail.com>
2017-10-19 16:18:59 -0200

    Criação de middleware para autenticação do token JWT em todas as requisições

---
+++ src/AppBundle/EventSubscriber/AuthSubscriber.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace AppBundle\EventSubscriber;
+
+use AppBundle\Controller\Api\v1\AuthController;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+use Firebase\JWT\JWT;
+
+class AuthSubscriber implements EventSubscriberInterface
+{
+    private $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+	public function onKernelController(FilterControllerEvent $event)
+    {
+        $controller = $this->getFirstController($event);
+
+        if (($controller instanceof AuthController)) {
+            return;
+        }
+        
+        $rawHeader = $event->getRequest()->headers->get('Authorization');
+
+        if (strpos($rawHeader, 'Bearer ') === false) {
+            $event->setController(
+                function() {
+                    return new JsonResponse(['message' => 'Unauthorized access.'], 401);
+                }
+            );
+        }
+
+        $headerWithoutBearer = str_replace('Bearer ', '', $rawHeader);
+        
+        $jwtSecretKey = $this->container->getParameter('secret');
+        
+        try {
+            JWT::$leeway = 60*60*24;
+            $decodedJWT = JWT::decode($headerWithoutBearer, $jwtSecretKey, ['HS256']);
+        }  catch (\Exception $e) {
+            $event->setController(
+                function() {
+                    return new JsonResponse(['message' => 'Unauthorized access.'], 401);
+                }
+            );
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::CONTROLLER => 'onKernelController',
+        );
+    }
+
+    private function getFirstController($event)
+    {
+        $controllers = $event->getController();
+
+        if (!is_array($controllers)) {
+            return null;
+        }
+
+        return $controllers[0];
+    }
+}
