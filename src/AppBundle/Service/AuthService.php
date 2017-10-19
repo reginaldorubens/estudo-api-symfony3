ef80f9ad13fee9215b6c7d17674e7af638aedce7
Reginaldo Rubens da Silva <reginaldorubens@gmail.com>
2017-10-19 14:50:51 -0200

    Conclusão da implementação do controller (e service) para autenticação de credenciais e geração do token JWT

--- src/AppBundle/Service/AuthService.php
+++ src/AppBundle/Service/AuthService.php
@@ -7,16 +7,21 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface as PasswordEncoder;
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+use Firebase\JWT\JWT;
 
 class AuthService
 {
 	private $encoder;
 	private $userService; 
+	private $container;
 
-	public function __construct(PasswordEncoder $encoder, UserService $userService)
+	public function __construct(PasswordEncoder $encoder, UserService $userService, 
+		Container $container)
 	{
 		$this->encoder = $encoder;
 		$this->userService = $userService;
+		$this->container = $container;
 	}
 
 	public function login(Request $request)
@@ -31,6 +36,10 @@ class AuthService
             return new JsonResponse(['message' => 'Unauthorized. Wrong password.'], 401);
         }
 
+        if (!$user->getActive()) {
+        	return new JsonResponse(['message' => 'Unauthorized. The user is inactive.'], 401);
+        }
+
         return new JsonResponse(['token' => $this->generateToken($user)], 200);
 	}
 
@@ -41,8 +50,29 @@ class AuthService
 
 	private function generateToken(UserInterface $user)
 	{
-		
+		$jsonObject = [
+            "iss" => "reginaldorubens",
+            "aud" => "https://github.com/reginaldorubens/test-api-restful-silex",
+            "iat" => time(), // Issued At Time
+            "nbf" => time(), // Not Before Time
+            "exp" => time()+60*60*24, // Expiration Time (24 hours)
+            "payload" => $this->formatTokenPayload($user)
+        ];
+
+        $jwtSecretKey = $this->container->getParameter('secret');
+        
+        $token = JWT::encode($jsonObject, $jwtSecretKey);
 
 		return $token;
+	}
+
+	private function formatTokenPayload($user)
+	{
+		$payload = [
+			'id' => $user->getId(),
+			'username' => $user->getUsername()
+		];
+
+		return $payload;
 	}
 }
